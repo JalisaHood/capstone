acd91fc902a421681c26ab45012a6bfa3f50c495
COR Students <corstudents@Matts-MBP-2.localdomain>
2018-11-02 13:45:13 -0400

    set up project

---
+++ scrpt.js
@@ -0,0 +1,15 @@
+var firstNum;
+var secondNum;
+var total;
+var plusClick;
+var minusClick;
+var divideClick;
+var decimalClick;
+
+function calc(x) {
+    math.txt.value=math.txt.value + x;
+}
+
+function insert(firstNum +" "+ 'secondNum'){
+    console.log= math.textview.value+num 
+}
